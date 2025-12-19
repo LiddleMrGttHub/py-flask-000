a0704e22ee755d6bff75b604a666110441fe17d2
M Liddle <mr.liddle+github@gmail.com>
2025-12-19 01:45:04 +0000

    0.0.0-3

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-@flask_app.route("/")
+@app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+if __name__ == '__main__':
+    app.run()
